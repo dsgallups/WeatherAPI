e32ee10512e731bb5c72a629ed1489dc807c9680
Daniel Gallups <dgallups@purdue.edu>
2021-04-28 12:18:37 -0400

    Added ascii art

--- main.c
+++ main.c
@@ -1,20 +1,88 @@
 #include <stdlib.h>
-#define NK_IMPLEMENTATION
+#include <stdio.h>
 
-/*
-    This is a basic GUI library for 
-
-    Github: https://github.com/Immediate-Mode-UI/Nuklear
-    Documentation: https://immediate-mode-ui.github.io/Nuklear/doc/nuklear.html
-    
-    Note: Tutorial to use this GUI is here - https://youtu.be/HtEMrQaDvm0 
-
-    ^ Dan G is currently watching through this tutorial and comitting on a separate branch to learn how to use this.
-*/
-#include "nuklear.h"
-
-
+void drawMenu();
 
 int main() {
+    drawMenu();
     return 0;
+}
+
+
+void drawMenu() {
+
+    system("clear");
+    char* menu = 
+    "#####################################################################################\n"
+    "#                                                                                   #\n"
+    "# (_)(_)(_|_______|_______|_______|_)   (_|_______|_____ \\   (_____ (___) (_______) #\n"
+    "#  _  _  _ _____   _______    _    _______ _____   _____) )   _____) ) _   ______   #\n"
+    "# | || || |  ___) |  ___  |  | |  |  ___  |  ___) |  __  /   (_____ ( | | (_____ \\  #\n"
+    "# | || || | |_____| |   | |  | |  | |   | | |_____| |  \\ \\    _____) )| |_ _____) ) #\n"
+    "#  \\_____/|_______)_|   |_|  |_|  |_|   |_|_______)_|   |_|  (______(_____|______/  #\n"
+    "#                                                                                   #\n"
+    "#                                                                                   #\n"
+    "#                                                                                   #\n"
+    "#                                  Enter Zipcode:                                   #\n"
+    "#                     |                                       |                     #\n"
+    "#                     |                                       |                     #\n"
+    "#                     |                                       |                     #\n"
+    "#                     |                                       |                     #\n"
+    "#                                                                                   #\n"
+    "#                                                                                   #\n"
+    "#                                                                                   #\n"
+    "#                                                                                   #\n"
+    "#                                                                                   #\n"
+    "#                                                                                   #\n"
+    "#####################################################################################\n";
+    printf("%s", menu);
+
+    /*
+     |                                       |
+         __    ___   ___    ___     _  
+        /. |  (__ ) / _ \  / _ \   / ) 
+       (_  _)  / /  \_  / ( (_) ) / _ \
+         (_)  (_/    (_/   \___/  \___/
+
+  ___
+ / _ \ 
+( (_) )
+ \___/ 
+  __   
+ /  )  
+  )(   
+ (__)  
+ ___   
+(__ \  
+ / _/  
+(____) 
+ ___   
+(__ )  
+ (_ \  
+(___/  
+  __   
+ /. |  
+(_  _) 
+  (_)  
+ ___   
+| __)  
+|__ \  
+(___/  
+  _    
+ / )   
+/ _ \  
+\___/  
+ ___   
+(__ )  
+ / /   
+(_/    
+ ___   
+( _ )  
+/ _ \  
+\___/  
+ ___   
+/ _ \  
+\_  /  
+ (_/   
+    */
 }
